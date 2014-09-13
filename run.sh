08fb757bc733c9825a513c832428374c3bda06c3
Marc Urben <aegnor@mittelerde.ch>
2014-09-13 11:16:57 +0200

    setting MMOFRIENDS_CFG if not already set

--- run.sh
+++ run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Getting script directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Saving origin path
+# Saving origin path.
 ORIGDIR=$(pwd)
 
 # Cleaning old .pyc files to not run into the "importing seems to work" trap again!
 find ${DIR} -name *.pyc -exec rm {} \;
 
-# Changing to the root path of th application
+# Changing to the root path of th application.
 cd ${DIR}
 
-# Actually starting the application
+# Checking if MMOFRIENDS_CFG is set. If not, use the provided example file.
+if [ -z "$MMOFRIENDS_CFG"]; then
+	echo "Setting MMOFRIENDS_CFG for you. Please use your own settings for production!"
+	export MMOFRIENDS_CFG="../dist/mmofriends.cfg.example"
+fi
+
+# Actually starting the application.
 python mmofriends.py
 
-# Changing back to origin path
+# Changing back to origin path.
 cd ${ORIGDIR}
