3e7f94bcb62c27a8f31bb4aa87825577fc2bbc94
Marc Urben <aegnor@mittelerde.ch>
2014-09-16 21:32:42 +0200

    update

--- mmofriends/mmobase/mmoutils.py
+++ mmofriends/mmobase/mmoutils.py
@@ -7,34 +7,14 @@ import os
 import io
 import json
 import yaml
-
-def timestampToString(ts):
-    return datetime.datetime.fromtimestamp(int(ts)).strftime('%d.%m.%Y %H:%M:%S')
-
-def bytes2human(n):
-    # http://code.activestate.com/recipes/578019
-    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-    prefix = {}
-    for i, s in enumerate(symbols):
-        prefix[s] = 1 << (i+1)*10
-    for s in reversed(symbols):
-        if int(n) >= prefix[s]:
-            value = float(n) / prefix[s]
-            return '%.1f%s' % (value, s)
-    return "%sB" % n
-
-def getLogger(level=logging.INFO):
-    myPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../')
-    logPath = os.path.join(myPath, 'log/mmofriends.log')
-    logging.basicConfig(filename=logPath, format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%d-%m %H:%M:%S', level=level)
-    console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
-    # formatter = logging.Formatter('%(levelname)-7s %(name)-25s| %(message)s')
-    # formatter = logging.Formatter("[%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)")
-    formatter = logging.Formatter("%(levelname)-7s %(message)s (%(filename)s:%(lineno)s)")
-    console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
-    return logging.getLogger(__name__)
+import sys
+import cgi
+import textwrap
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 class YamlConfig (object):
     def __init__(self, filename = None):
@@ -80,3 +60,107 @@ class JsonConfig (object):
     def set_values(self, values):
         self.values = values
 
+def timestampToString(ts):
+    return datetime.datetime.fromtimestamp(int(ts)).strftime('%d.%m.%Y %H:%M:%S')
+
+def bytes2human(n):
+    # http://code.activestate.com/recipes/578019
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i+1)*10
+    for s in reversed(symbols):
+        if int(n) >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+def getLogger(level=logging.INFO):
+    myPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../')
+    logPath = os.path.join(myPath, 'log/mmofriends.log')
+    logging.basicConfig(filename=logPath, format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%d-%m %H:%M:%S', level=level)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('%(levelname)-7s %(name)-25s| %(message)s')
+    # formatter = logging.Formatter("[%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)")
+    formatter = logging.Formatter("%(levelname)-7s %(message)s (%(filename)s:%(lineno)s)")
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    return logging.getLogger(__name__)
+
+def load_file(app, msgRoot, filename):
+    fp = open(os.path.join(app.root_path, 'static/img/', filename), 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    msgImage.add_header('Content-Disposition', 'inline', filename=filename)
+    msgImage.add_header('Content-ID', '<' + os.path.splitext(filename)[0] + '@mmofriends.local>')
+    msgRoot.attach(msgImage)
+
+def send_email(app, msgto, msgsubject, msgname):
+    try:
+        msgRoot = MIMEMultipart('related', type="text/html")
+        msgRoot['Subject'] = app.config['EMAIL_SUBJECT']
+        msgRoot['From'] = app.config['EMAIL_MSGFROM']
+        msgRoot['To'] = msgto
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
+        msgRoot.add_header('reply-to', app.config['EMAIL_REPLYTO'])
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgtext = u"""<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>%s</title>
+        <style type="text/css" media="screen">
+            body {
+                margin: 0;
+                padding: 0;
+            }
+            #background { 
+                left: 0px; 
+                top: 0px; 
+                position: relative; 
+                margin-left: auto; 
+                margin-right: auto; 
+                width: 600px;
+                height: 500px; 
+                overflow: hidden;
+                z-index:0;
+            }
+            #logo { 
+                left: 0px; 
+                top: 0px; 
+                position: absolute; 
+                width: 600px;
+                height: 150px;
+                z-index:2;
+            } 
+            #
+        </style>
+    </head>
+    <body>
+        <div id="background">
+            <div id="logo"><img src="cid:logo@mmofriends.local"></div>
+            <div ig="content">%s</div>
+        </div>
+   </body>
+   </html>""" % (app.config['EMAIL_SUBJECT'], msgtext.replace('\n', '<br />').encode('ascii', 'xmlcharrefreplace'))
+
+        newplaintext = ""
+        for line in msgtext.split("\n"):
+            newplaintext += "\n".join(textwrap.wrap(line)) + "\n"
+
+        part1 = MIMEText(newplaintext.replace('\n', '\r\n').encode("UTF-8"), 'plain', 'UTF-8')
+        part2 = MIMEText(htmltext.replace('\n', '\r\n').encode('UTF-8'), 'html', 'UTF-8')
+        msgAlternative.attach(part1)
+        msgAlternative.attach(part2)
+
+        load_file(app, msgRoot, 'email_header.png')
+
+        s = smtplib.SMTP(app.config['EMAILSERVER'])
+        s.sendmail(app.config['EMAIL_MSGFROM'], msgto, msgRoot.as_string())
+        s.quit()
+        return True
+    except Exception as e:
+        print 'Email ERROR: ' + str(e) + ' on line ' + str(sys.exc_traceback.tb_lineno)
+        return False
