057e499e6f126c0b2f06f8596b0f1a9108507556
Marc Urben <aegnor@mittelerde.ch>
2014-10-08 22:27:37 +0200

    now showing friends to add

--- mmofriends/mmobase/blizznetwork.py
+++ mmofriends/mmobase/blizznetwork.py
@@ -423,33 +423,4 @@ class BlizzNetwork(MMONetwork):
 
     def findPartners(self):
         self.log.debug("Searching for new partners to play with")
-        return ( False, "Network not yet programmed")
-        return ( True, {'id': 'someId',
-                        'mmoid': internalId,
-                        'nick': 'nickName',
-                        'state': 'State',
-                        'netHandle': self.handle,
-                        'networkText': 'Product',
-                        'networkImgs': [{
-                            'type': 'network',
-                            'name': self.handle,
-                            'title': self.name
-                        },{
-                            'type': 'cache',
-                            'name': 'gameIconPath',
-                            'title': 'gameName'
-                        },{
-                            'type': 'cache',
-                            'name': 'mapIconPath',
-                            'title': 'mapName'
-                        }],
-                        'friendImgs': [{
-                            'type': 'cache',
-                            'name': 'rankIconPath',
-                            'title': 'rankIcon'
-                        },{
-                            'type': 'cache',
-                            'name': 'someImagePath',
-                            'title': 'someImage'
-                        }]
-                    })
+        return ( True, self.getPartners() )
