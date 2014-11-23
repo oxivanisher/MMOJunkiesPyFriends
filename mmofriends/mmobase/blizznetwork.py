7561d6e2d371848b86b0ccf4c80025fcb9bc7744
Marc Urben <aegnor@mittelerde.ch>
2014-11-23 11:37:00 +0100

    working

--- mmofriends/mmobase/blizznetwork.py
+++ mmofriends/mmobase/blizznetwork.py
@@ -307,6 +307,7 @@ class BlizzNetwork(MMONetwork):
 
         okCount = 0
         nokCount = 0
+        print self.getNetworkLinks()
         for link in self.getNetworkLinks():
             logger.debug("[%s] Updating user resources for userid %s" % (self.handle, link['user_id']))
             if link['network_data']:
