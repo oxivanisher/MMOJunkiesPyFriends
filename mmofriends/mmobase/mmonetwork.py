eba5d0f401a7f75887fbbe0ce418ba8d7b1610ae
Marc Urben <aegnor@mittelerde.ch>
2014-11-23 11:42:01 +0100

    working

--- mmofriends/mmobase/mmonetwork.py
+++ mmofriends/mmobase/mmonetwork.py
@@ -186,6 +186,7 @@ class MMONetwork(object):
         else:
             logger.debug("[%s] Getting all network links" % (self.handle))
             for link in db.session.query(MMONetLink).filter_by(network_handle=self.handle):
+                logger.debug("aa")
                 netLinks.append({'network_data': link.network_data, 'linked_date': link.linked_date, 'user_id': link.user_id, 'id': link.id})
         logger.debug("returning %s" % netLinks)
         return netLinks
