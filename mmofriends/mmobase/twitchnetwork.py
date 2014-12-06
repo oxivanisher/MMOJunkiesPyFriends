8949b79f0f7fbfa593511a5074b7c279f11c64b5
Marc Urben <aegnor@mittelerde.ch>
2014-12-06 03:48:48 +0100

    working

--- mmofriends/mmobase/twitchnetwork.py
+++ mmofriends/mmobase/twitchnetwork.py
@@ -139,10 +139,10 @@ class TwitchNetwork(MMONetwork):
             self.getCache("streams")
             (ret, stream) = self.queryTwitchApi("/streams/%s" % channel['name'], accessToken)
             if ret and len(stream):
-                print stream
-                if 'stream' in stream:
-                    if 'preview' in stream['stream']:
-                        stream['stream']['preview'] = stream['stream']['preview'].replace('http://', '//')
+                if stream:
+                    if 'stream' in stream:
+                        if 'preview' in stream['stream']:
+                            stream['stream']['preview'] = stream['stream']['preview'].replace('http://', '//')
                 if 'error' in channel.keys():
                     logger.warning("[%s] Unable to fetch stream for %s: %s (%s)" % (self.handle, userNick, stream['error'], stream['message']))
                     return (False, "Unable to update resources for %s: %s (%s)" % (userNick, stream['error'], stream['message']))
