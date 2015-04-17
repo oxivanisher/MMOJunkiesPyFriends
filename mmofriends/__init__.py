47a452c0b8f44f187728020ec76bb91f2fac8e5b
Marc Urben <aegnor@mittelerde.ch>
2015-04-17 11:07:07 +0200

    working

--- mmofriends/__init__.py
+++ mmofriends/__init__.py
@@ -553,24 +553,26 @@ def getNetworksLinkData(request = None):
         netLinked = False
 
         for link in fetchNetworkLinksData[netKey]:
-            netLinked = True
-            if link['network_data']:
-                linkedNetworks.append({'id': netKey,
-                                       'name': net.name,
-                                       'description': net.description,
-                                       'handle': net.handle,
-                                       'icon': url_for('get_image', imgType='network', imgId=net.handle),
-                                       'unlinkLink': url_for('network_unlink', netHandle=net.handle, netLinkId=link['id']),
-                                       'linked_date': timestampToString(link['linked_date']) })
-            else:
-                reLinkNetworks.append({ 'id': netKey,
-                                        'icon': url_for('get_image', imgType='network', imgId=net.handle),
-                                        'name': net.name,
-                                        'handle': net.handle,
-                                        'description': net.description,
-                                        'linkData': net.getLinkHtml() })
+            if net.getLinkHtml():
+                netLinked = True
 
-        if not netLinked:
+                if link['network_data']:
+                    linkedNetworks.append({'id': netKey,
+                                           'name': net.name,
+                                           'description': net.description,
+                                           'handle': net.handle,
+                                           'icon': url_for('get_image', imgType='network', imgId=net.handle),
+                                           'unlinkLink': url_for('network_unlink', netHandle=net.handle, netLinkId=link['id']),
+                                           'linked_date': timestampToString(link['linked_date']) })
+                else:
+                    reLinkNetworks.append({ 'id': netKey,
+                                            'icon': url_for('get_image', imgType='network', imgId=net.handle),
+                                            'name': net.name,
+                                            'handle': net.handle,
+                                            'description': net.description,
+                                            'linkData': net.getLinkHtml() })
+
+        if not netLinked and net.getLinkHtml():
             linkNetwork.append({ 'id': netKey,
                                  'icon': url_for('get_image', imgType='network', imgId=net.handle),
                                  'name': net.name,
