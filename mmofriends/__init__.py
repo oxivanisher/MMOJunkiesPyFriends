0d8ae0f70c29bf436dabd06dac4e368bb1e8a72e
Marc Urben <aegnor@mittelerde.ch>
2015-05-16 14:08:06 +0200

    working on paypal

--- mmofriends/__init__.py
+++ mmofriends/__init__.py
@@ -1410,8 +1410,8 @@ def paypal_webhook():
     log.info(request.form)
     log.info(verify_string)
 
-    # with contextlib.closing(urllib.urlopen(IPN_URLSTRING, data=verify_string.encode('utf-8'))) as paypal_verify_request:
-    with contextlib.closing(urllib.urlopen(IPN_URLSTRING, data=verify_string)) as paypal_verify_request:
+    with contextlib.closing(urllib.urlopen(IPN_URLSTRING, data=verify_string.encode('utf-8'))) as paypal_verify_request:
+    # with contextlib.closing(urllib.urlopen(IPN_URLSTRING, data=verify_string)) as paypal_verify_request:
         # response_string = paypal_verify_request.read()
         # if response_string != 'VERIFIED':
         #     raise ValueError('Did not receive expected IPN confirmation from PayPal. String is: %s' % response_string)
