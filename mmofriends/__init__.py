a499be9f755311b8e086b6d118fe1b7e1ef1fc0b
Marc Urben <aegnor@mittelerde.ch>
2015-10-01 15:05:05 +0200

    working on lang redirect

--- mmofriends/__init__.py
+++ mmofriends/__init__.py
@@ -541,7 +541,7 @@ def set_lang(language=None, path = None):
         path = url_for('index')
     session['displayLanguage'] = language
     log.info("[System] Set lang to %s and redirect to %s" % (session['displayLanguage'], path))
-    return redirect(request.url_root + path)
+    return redirect((request.url_root + path).replace('//', '/'))
 
 # support routes
 @app.route('/Images/<imgType>/', methods = ['GET', 'POST'])
